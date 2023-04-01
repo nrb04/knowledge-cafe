429f88878052cd3753e71148a3bfe10d69be7b74
nrb04 <nredoyan@gmail.com>
2023-04-01 14:22:24 +0600

    work finish desgin off

--- src/componads/Card.jsx
+++ src/componads/Card.jsx
@@ -1,9 +1,12 @@
+import { useState } from "react";
+import Cart from './Cart';
 
 
-
-const Card =(props)=> {
-const { title, img, pimg, description, id } = props.card;
-const handleAddToCart = props.handleAddToCart;
+const Card = (props) => {
+const { reading, title, img, pimg, description, id } = props.card;
+//  const handleAddToCart = props.handleAddToCart;
+ 
+  
  
     return (
         
@@ -21,19 +24,14 @@ const handleAddToCart = props.handleAddToCart;
         className="mb-2 text-xl font-medium leading-tight text-neutral-800 dark:text-neutral-50">
             {title}
                     </h5>
-                  
-          <button onClick={() => props.handleAddToCart(props.card, id )} className='btn-cart'>
+          <h4 >{reading} min read </h4>          <button onClick={() => props.handleAddToCart(props.card, id )} className='btn-cart'>
                 Add to Cart
                 </button>
 
       <p className="mb-4 text-base text-neutral-600 dark:text-neutral-200">
         {description}
       </p>
-      <button
-        type="button"
-        className="" >
-        Button
-      </button>
+         <a onClick={()=>props.handleReadTime(props.card)} className="text-left mt-8 underline text-blue-500" ><button>Mark as read</button></a>
         </div>
           
   </div>
--- src/componads/Cards.jsx
+++ src/componads/Cards.jsx
@@ -6,6 +6,8 @@ import Cart from './Cart';
 const Cards = () => {
     const [cards, setCards] = useState([]);
     const [cart, setCart] = useState([]);
+    const [readTime, setReadTime] = useState([]);
+    
  
     useEffect(() => {
         fetch('public.json')
@@ -15,19 +17,26 @@ const Cards = () => {
     const handleAddToCart = (card) => {
        const newCart = [...cart, card];
         setCart(newCart);
- }
+    }
+   const handleReadTime = (reading) => {
+    const newReadTime = [...readTime, reading];
+    setReadTime(newReadTime);
+  };
     return (
         <div className="flex  mx-20 ">
           <div className="px-5 w-3/4  bg-white shadow-lg dark:bg-neutral-700" >  {
                 cards.map(card => <Card key={card.id}
                     card={card}
                     handleAddToCart={handleAddToCart}
-                    ></Card>
+                   handleReadTime={handleReadTime}
+                ></Card>
                     )
             }
+               
+             
             </div>
             <div className="w-1/4 bg-white shadow-lg dark:bg-neutral-700" >
-                    <Cart cart={cart}></Cart>
+                    <Cart cart={cart} reading={readTime} ></Cart>
       
             </div>
         </div>
